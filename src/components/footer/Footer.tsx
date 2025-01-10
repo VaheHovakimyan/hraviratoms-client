78f7f1e5125f3922d3936ffcd76d72236746b7a6
Vahe <anihovakimyan001@gmail.com>
2025-01-10 19:47:05 +0400

    update footer.scss file

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,9 @@ import './Footer.scss';
 const Footer: FC = () => {
   return (
     <div className="footer_main">
-      <div className="footer_logo">White</div>
+      <div className="footer_logo">White
+        <img src=""/>
+      </div>
     </div>
   )
 }
