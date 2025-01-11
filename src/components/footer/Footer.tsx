6afe29c2217605b5bf74b788f50c2807b8c51ca5
Vahe <anihovakimyan001@gmail.com>
2025-01-11 13:11:43 +0400

    update footer.scss file

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,30 @@
 import { FC } from "react";
+import footer_logo from '../../images/logo/logo_hraviratoms.svg';
+
 import './Footer.scss';
 
 const Footer: FC = () => {
   return (
     <div className="footer_main">
       <div className="footer_logo">White
-        <img src=""/>
+        <img src={footer_logo} alt="" />
+      </div>
+      <p className="tekst_hraviratoms">Hraviratoms.com
+
+      </p>
+
+      <div className="number">
+        <div>
+          <img src={footer_logo} alt="" width="24px" height="24px"/>
+          <p>+374 77 49 75 15</p>
+        </div>
+        <div>
+          <img src={footer_logo} alt="" width="24px" height="24px"/>
+          <p>+374 77 49 75 15</p></div>
+      </div>
+
+      <div className="mail_hraviratoms">
+        <img src={footer_logo} alt="" width="24px" height="24px"/>hraviratoms.com@gmail.com
       </div>
     </div>
   )
