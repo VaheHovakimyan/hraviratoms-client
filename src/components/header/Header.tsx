3b76b72da70b964e3220b6b76dab623070e9f9bc
Vahe <anihovakimyan001@gmail.com>
2025-01-15 22:43:14 +0400

    correct footer_logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,39 +3,43 @@ import question_background from '../../images/question_background.png';
 import 'question.scss';
 
 const Header: FC = () => {
-    return (
-        <div className="question_main">
-           <div className="question_logo">
-             <img src={question_background} alt="question_background"/>
-           </div>
-
-           <div>
-            <p className="header_text">
-            Պատասխան կարող եք ուղարկել մինչև 15.01.2025թ
-            </p>
-           </div>
-
-           <div>
-            <p>Անուն Ազգանուն</p>
-            {/* contact */}
-           </div>
-
-           <div>
-            <p>Հեռախոսահամար </p>
-            {/* contact */}
-           </div>
-
-           <p>Արարողությանը ներկա եք լինելու?</p>
-
-           <div>
-            {/* input, label*/}
-           </div>
-
-           <div>
-            <button>Ուղարկել</button>
-           </div>
+  return (
+    <div className="question_main">
+      <div className="question_content">
+        <div className="question_logo">
+          <img src={question_background} alt="question_background" />
         </div>
-    )
+
+        <div>
+          <p className="header_text">
+            Պատասխան կարող եք ուղարկել մինչև 15.01.2025թ
+          </p>
+        </div>
+
+        <div>
+          <p>Անուն Ազգանուն</p>
+          {/* contact */}
+        </div>
+
+        <div>
+          <p>Հեռախոսահամար </p>
+          {/* contact */}
+        </div>
+
+        <div>
+          <p>Արարողությանը ներկա եք լինելու?</p>
+        </div>
+
+        <div>
+          {/* input, label*/}
+        </div>
+
+        <div>
+          <button>Ուղարկել</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Header;
