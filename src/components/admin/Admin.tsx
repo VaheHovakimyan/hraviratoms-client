d38f3cc8c132c32865694cc421537d06572889c2
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2025-02-23 22:42:44 +0400

    add responsive

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -2,8 +2,9 @@ import { FC, useEffect, useState } from "react";
 import Footer from "../footer/Footer";
 import './Admin.scss';
 import { getGuests } from "../../api/invite-api";
+import Loader from "../loader/Loader";
 
-interface IGuestData{
+interface IGuestData {
   _id: string;
   name?: string;
   phone: string;
@@ -22,28 +23,37 @@ const Admin: FC = () => {
   }, []);
 
   return (
-    <div>
+    <>
       <h3>Գեղամ Հակոբյանի հյուրերի ցուցակը</h3>
-      <table>
-        <tr>
-          <th className="container">Համար</th>
-          <th className="container">Անուն Ազգանուն</th>
-          <th className="container">Հեռախոսահամար</th>
-          <th className="container">Ներկա Կգտնվի՞</th>
-        </tr>
-        {guests.map((item, index) => {
+      <div className="table">
+        <div className="tr">
+          <div className="admin_param" id="guest_id">No.</div>
+          <div className="admin_param" id="guest_name">Անուն Ազգանուն</div>
+          <div className="admin_param" id="guest_phone">Հեռախոսահամար</div>
+          <div className="admin_param" id="guest_isVisting">Ներկա Կգտնվի՞</div>
+        </div>
+        {guests.length ? guests.map((item, index) => {
           return (
-            <tr key={item._id}>
-              <td>{index + 1}</td>
-              <td>{item?.name || ''}</td>
-              <td>{item?.phone || ''}</td>
-              <td>{item.isVisiting === "Yes" ? 'Այո' : 'Ոչ'}</td>
-            </tr>
+            <div key={item._id} className="tr">
+              <div id="guest_id" className="td">
+                <p className="td_p">{index + 1}</p>
+              </div>
+              <div id="guest_name" className="td">
+                <p className="td_p">{item?.name || ''}</p>
+              </div>
+              <div id="guest_phone" className="td">
+                <p className="td_p">{item?.phone || ''}</p>
+              </div>
+              <div id="guest_isVisting" className="td">
+                <p className="td_p">{item.isVisiting === "Yes" ? 'Այո' : 'Ոչ'}</p>
+              </div>
+            </div>
           )
-        })}
-      </table>
+        }) : <Loader message="Խնդրում ենք սպասել․․․" />}
+
+      </div>
       <Footer />
-    </div>
+    </>
   )
 }
 
